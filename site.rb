e5544178e89c3689c3770599038386b90837db6f
Andrew Smith <u1002335@utah.edu>
2016-05-14 14:23:11 +0000

    Add 'italian' route

--- site.rb
+++ site.rb
@@ -1,7 +1,10 @@
-require 'Sinatra'
+require 'sinatra'
+require 'bundler/setup'
 ENV['PORT'] ||= '4000'
 set :port, ENV['PORT']
 set :bind, '0.0.0.0'
 get '/' do
-  erb :index
+  "erb :index"
+get '/italian' do
+  "Ciao!"
 end
